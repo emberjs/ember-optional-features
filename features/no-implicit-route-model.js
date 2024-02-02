e381c2873a5786555dca8ebb89dac74ccdc32ca1
Aaron Chambers <aaron.chambers@hey.com>
2024-02-02 15:53:37 +0000

    Add feature for no-implicit-route-model

---
+++ features/no-implicit-route-model.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+'use strict';
+
+module.exports = {
+  description: `Removes the default record loading behaviour on Ember's Route.`,
+  url: 'https://github.com/emberjs/rfcs/pull/774',
+  default: false,
+  since: '5.8.0',
+};
