59ffc91e8ac88b5951d87542a36355d4cb988d36
Aaron Chambers <aaron.chambers@hey.com>
2024-02-07 15:30:33 +0000

    Update no-implicit-route-model.js

--- features/no-implicit-route-model.js
+++ features/no-implicit-route-model.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   description: `Removes the default record loading behaviour on Ember's Route.`,
-  url: 'https://github.com/emberjs/rfcs/pull/774',
+  url: 'https://rfcs.emberjs.com/id/0774-implicit-record-route-loading',
   default: false,
   since: '5.8.0',
 };
