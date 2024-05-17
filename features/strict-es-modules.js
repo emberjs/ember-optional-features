1e1e0d3fcfddad97157de080e2bc21654a5741d5
Edward Faulkner <edward@eaf4.com>
2024-05-17 11:07:08 -0400

    introduce strict-es-modules

---
+++ features/strict-es-modules.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  description: 'Use only spec-compliant ES modules',
+  url: 'https://github.com/emberjs/rfcs/pull/938',
+  default: false,
+  since: '5.9.0', // TODO: update to real release
+};
