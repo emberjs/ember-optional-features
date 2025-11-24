15bf4134f51e364abac35bec44c2a0b811ba52e3
Edward Faulkner <edward@eaf4.com>
2025-11-24 11:15:43 -0500

    Introducing use-ember-modules flag

---
+++ features/use-ember-modules.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+'use strict';
+
+module.exports = {
+  description:
+    'Use the published ES modules in ember-source, instead of the legacy AMD bundles.',
+  url: 'https://github.com/emberjs/rfcs/pull/1101',
+  default: false,
+  since: '6.10.0',
+};
